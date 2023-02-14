d806237581c29df68bea7f1d1e42a3e9da6b7a2c
Bilal Ahad <engr.bilalahad2001@gmail.com>
2023-02-14 23:23:07 +0500

    screenwidth commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const[Value,setValue]=useState(0);
+  const[Width,setWidth]=useState(window.screen.width);
+  useEffect(()=>{
+    window.addEventListener('resize',screenwidth)
+  })
+  const screenwidth=()=>{
+    setWidth(window.screen.width);
+  }
   
   const increment=()=>{
     setValue(Value+1);
@@ -19,11 +26,14 @@ function App() {
     document.title=(`React ${Value}`);
   },[Value])
 
+
+
   
   return (
     <>
     
     <div className="grid place-items-center h-[100vh] bg-gray-800">
+    <p className='text-xl text-white'>{(`your screen width = ${Width}`)}</p>
     <h1 className='text-2xl font-bold text-center shadow-gray-600 bg-white rounded-3xl uppercase italic mx-2 shadow-2xl p-5'>increment And Decrement Buttons Through React hooks</h1>
      <section className="gap-12 grid grid-flow-col justify-center  bg-gray-300 border-x-4 border-black  px-5 py-16 rounded-3xl">
      <div><button className="text-6xl font-bold  hover:text-7xl hover:brightness-75 text-center " onClick={increment} >+</button></div>
